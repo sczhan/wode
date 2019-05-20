8f9ae76de9f46c0c2a7f49659cf77f82b6c6cf47
sczhan <5481493@qq.com>
2019-05-20 23:58:50 +0800

    屏保(完成,但要修改)

--- Tkinter_屏保.py
+++ Tkinter_屏保.py
@@ -1,4 +1,3 @@
-
 # Tkinter项目-屏保
 # 项目分析:
 # 1.屏保可以自己启动,也可以手动启动
@@ -15,10 +14,12 @@
 import random
 import tkinter
 
+
 class Ball(object):
     """
     定义运动的球的类
     """
+
     def __init__(self, canvas, scrnwidth, scrnheight):
         """
         :param canvas: 画布  所有内容都应该在画布上呈现出来,此处通过变量传入
@@ -27,9 +28,10 @@ class Ball(object):
         """
         # 球出现的初始位置要随机,此位置表示的球的圆心
         # xpos表示位置的x坐标
-        self.xpos = random.randint(10, int(scrnwidth)-20)
+        self.xpos = random.randint(10, int(scrnwidth) - 20)
         # ypos表示位置的y坐标
-        self.ypos = random.randint(30, int(scrnheight)-10)
+        self.ypos = random.randint(30, int(scrnheight) - 10)
+        self.canvas = canvas
 
         # 定义球运动的速度
         # 模拟运动 不断的擦掉原来的画,然后在一个新的地方再从新绘制
@@ -52,30 +54,57 @@ class Ball(object):
         # 在某些系统中, 之间用英文单词表表示也可以,比如red, green
         # 此处用lambda表达式
         c = lambda: random.randint(0, 255)
-        self.color = "#%02x%02x%02%" % (c(), c(), c())
+        self.color = "#%02x%02x%02x" % (c(), c(), c())
 
-        def create_ball(self):
-            """
-            用构造函数定义的变量值, 在canvas上画一个球
-            :param self:
-            :return:
-            """
-            # tkinter没有画圆形函数
-            # 只要一个花椭圆函数,花椭圆需要定义两个坐标,
-            # 在一个长方形内画椭圆,我们只需要定义长方形左上角和右下角就好
-            # 求两个坐标的方法是, 已知圆心的坐标,则圆心坐标减去半径能求出左上角,加上半径能求出右下角坐标
-            x1 = self.xpos - self.radius
-            # 继续球y1, x2, y2
-            y1 = self.ypos - self.radius
-            x2 = self.xpos + self.radius
-            y2 = self.ypos + self.radius
+    def create_ball(self):
+        """
+        用构造函数定义的变量值, 在canvas上画一个球
+         :param self:
+         :return:
+        """
+        # tkinter没有画圆形函数
+        # 只要一个花椭圆函数,花椭圆需要定义两个坐标,
+        # 在一个长方形内画椭圆,我们只需要定义长方形左上角和右下角就好
+        # 求两个坐标的方法是, 已知圆心的坐标,则圆心坐标减去半径能求出左上角,加上半径能求出右下角坐标
+        x1 = self.xpos - self.radius
+        # 继续球y1, x2, y2
+        y1 = self.ypos - self.radius
+        x2 = self.xpos + self.radius
+        y2 = self.ypos + self.radius
 
-            # 再有两个对角坐标的前提下,可以进行画圆
-            # fill 表示填充颜色
-            # outline是外围边框颜色
-            self.item = self.canvas.create_oval(x1, y1, x2, y2,\
-                                                fill=self.color,\
-                                                outline=self.color)
+        # 再有两个对角坐标的前提下,可以进行画圆
+        # fill 表示填充颜色
+        # outline是外围边框颜色
+        self.item = self.canvas.create_oval(x1, y1, x2, y2,\
+                                            fill=self.color,\
+                                            outline=self.color)
+
+    def move_ball(self):
+        # 移动球的时候,需要控制球的方向
+        # 每次移动后,球都有一个新的坐标
+        self.xpos += self.xvelocity
+        self.ypos += self.yvelocity
+
+        # 以下判断是否会撞墙
+        # 撞了南墙就要回头
+        # 注意撞墙的算法判断
+        if self.xpos + self.radius >= self.scrnwidth:
+            # 撞到右边的墙
+            self.xvelocity = - self.xvelocity
+            # self.xvelocity *= 1
+        # 同理可以判断撞别的墙
+        if self.radius + self.ypos >= self.scrnheight:
+            self.yvelocity = -self.yvelocity
+
+        elif self.xpos < self.radius:
+            self.xvelocity = self.xvelocity
+
+        elif self.radius > self.ypos:
+            self.yvelocity = -self.yvelocity
+
+        # 在画布上挪动图画
+        self.canvas.move(self.item, self.xvelocity, self.yvelocity)
+
 
 
 class ScreenSaver():
@@ -83,4 +112,51 @@ class ScreenSaver():
     定义屏保的类
     可以被启动
     """
-    pass
+    # 如何装随机产生的球
+    balls = list()
+
+    def __init__(self):
+        # 每次启动球的数量是随机的
+        self.num_balls = random.randint(6, 15)
+        self.root = tkinter.Tk()
+
+        # 取消边框
+        self.root.overrideredirect(1)
+
+        # 任何鼠标移动都需要取消
+        self.root.bind("<Motion>", self.myquit)
+        # 同理,按动任何键盘都需要退回屏保
+
+        # 得到屏幕大小规格
+        w, h = self.root.winfo_screenwidth(), self.root.winfo_screenheight()
+
+        # 创建画布,包括画布分归属,规格
+        self.canvas = tkinter.Canvas(self.root, width=w, height=h)
+        self.canvas.pack()
+
+        # 在画布上画球
+        for i in range(self.num_balls):
+            ball = Ball(self.canvas, scrnwidth=w, scrnheight=h)
+            ball.create_ball()
+            self.balls.append(ball)
+
+        self.run_screen_saver()
+        self.root.mainloop()
+
+    def run_screen_saver(self):
+        for ball in self.balls:
+            ball.move_ball()
+
+        # after是200毫秒后启动一个函数,需要启动函数的是第二个参数
+        self.canvas.after(200, self.run_screen_saver)
+
+    def myquit(self, e):
+        # 此处只是利用了事件处理机制
+        # 实际上并不是关心事件的类型
+        # 作业:此屏保程序一旦扩展成,一旦捕获到事件,则判断屏保不退出
+        # 显示一个Button,Button上显示事件类型,点击Button后屏保才退出
+        self.root.destroy()
+
+
+if __name__ == "__main__":
+     ScreenSaver()
